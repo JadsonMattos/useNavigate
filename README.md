672a9e37273ad7ac2b11c253af513654c08479ee
JadsonMattos <98970595+JadsonMattos@users.noreply.github.com>
2023-07-14 09:25:48 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# useNavigate
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-fkwuig)
---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from 'react-router-dom';
+import Login from './pages/login';
+import Tasks from './pages/tasks';
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/tasks" element={<Tasks />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
